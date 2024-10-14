762f63da7c87267e056a370116dc57ae5a3fd47f
halvor <halvorbmundal@gmail.com>
2024-10-14 12:57:14 +0200

    Dont try to push to `window._sz` if `window._sz` is undefined

--- src/components/details-page/components/details-page/AccessRequestButton.tsx
+++ src/components/details-page/components/details-page/AccessRequestButton.tsx
@@ -43,7 +43,7 @@ export function AccessRequestButton({
       <SC.AccessRequest>
         <Button
           onClick={() => {
-            // trackAccessRequest();
+            trackAccessRequest();
             getAppliactionLink(location.pathname).then(url => {
               window.location.href = url;
             });
